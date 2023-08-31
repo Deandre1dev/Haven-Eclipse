600b4d9a46db0213caee1b88849e06ddd55ec0ec
Deandre <deandrewright333@gmail.com>
2023-08-30 16:27:42 -1000

    final commit

--- mySite/settings.py
+++ mySite/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=vu1abw_sz(+_t@#3@5pevi3%5bd@*9sq*-i)(2c(&)v7)kogy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
